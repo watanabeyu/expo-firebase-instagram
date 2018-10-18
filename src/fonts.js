5d4c8218e6747fd06fc1e40c61b291e196e683ed
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-10-18 14:20:39 +0900

    3.2.3

---
+++ src/fonts.js
@@ -0,0 +1,15 @@
+const notoSansThin = require('app/assets/fonts/NotoSansJP-Thin.otf');
+const notoSansLight = require('app/assets/fonts/NotoSansJP-Light.otf');
+const notoSansRegular = require('app/assets/fonts/NotoSansJP-Regular.otf');
+const notoSansMedium = require('app/assets/fonts/NotoSansJP-Medium.otf');
+const notoSansBold = require('app/assets/fonts/NotoSansJP-Bold.otf');
+
+const fonts = {
+  'noto-sans-thin': notoSansThin,
+  'noto-sans-light': notoSansLight,
+  'noto-sans-regular': notoSansRegular,
+  'noto-sans-medium': notoSansMedium,
+  'noto-sans-bold': notoSansBold,
+};
+
+export default fonts;
