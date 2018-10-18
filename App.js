dc9180a571c4ec3f0b14b81417d725203f80c5b1
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-10-18 14:20:39 +0900

    App.jsを編集して動くようにする

--- App.js
+++ App.js
@@ -1,62 +1,53 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import { View, Text } from 'react-native';
+import {
+  AppLoading,
+  Asset,
+  Font,
+} from 'expo';
+/* from app */
+import fonts from 'app/src/fonts';
+import images from 'app/src/images';
 
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
-
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    }
+  static defaultProps = {
+    skipLoadingScreen: false,
   }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
+  constructor(props) {
+    super(props);
 
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
+    this.state = {
+      isLoadingComplete: false,
+    };
+  }
 
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
+  loadResourcesAsync = async () => {
+    /* asset */
+    await Asset.loadAsync(Object.keys(images).map(key => images[key]));
+    /* font */
+    await Font.loadAsync(fonts);
+
+    return true;
+  }
+
+  render() {
+    const { isLoadingComplete } = this.state;
+    const { skipLoadingScreen } = this.props;
+
+    if (!isLoadingComplete && !skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.loadResourcesAsync}
+          onError={error => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      );
+    }
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
---
+++ src/images.js
@@ -0,0 +1,7 @@
+const icon = require('app/assets/images/icon.png');
+
+const images = {
+  icon,
+};
+
+export default images;
