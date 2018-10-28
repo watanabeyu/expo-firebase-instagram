26a0bfd2a7d9a8d75aec7450acea5a1047ddd423
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-10-28 17:06:34 +0900

    3.2.6-04

---
+++ src/screens/UserScreen/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import { Constants } from 'expo';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Constants.manifest.extra.backgroundColor,
+  },
+  header: {
+    alignItems: 'center',
+    padding: 12,
+  },
+  avatar: {
+    marginVertical: 12,
+  },
+  name: {
+    textAlign: 'center',
+    marginBottom: 12,
+  },
+  thumbnails: {
+    flex: 1,
+  },
+  file: {
+    flex: 1,
+    width: Dimensions.get('window').width / 3,
+    height: Dimensions.get('window').width / 3,
+    marginRight: 1,
+    marginBottom: 1,
+    backgroundColor: '#efefef',
+  },
+});
+
+export default styles;
