557d8db3462e0d6b187d7fc70c50b99444bfd2fd
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-10-28 18:10:01 +0900

    5.1.4 Android

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,15 @@ import { Constants } from 'expo';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 
+// symbol polyfills
+global.Symbol = require('core-js/es6/symbol');
+require('core-js/fn/symbol/iterator');
+
+// collection fn polyfills
+require('core-js/fn/map');
+require('core-js/fn/set');
+require('core-js/fn/array/find');
+
 class Firebase {
   constructor(config = {}) {
     firebase.initializeApp(config);
