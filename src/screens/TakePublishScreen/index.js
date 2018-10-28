20200d502eddfc1ba0d465e9b8f007261c8bd49d
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-10-28 11:21:55 +0900

    4.2.4

---
+++ src/screens/TakePublishScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class TakePublishScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Hello Take Publish Screen</Text>
+      </View>
+    );
+  }
+}
