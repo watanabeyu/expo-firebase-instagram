cb9dd375f1bdee06b29b2144f4ccbad602d27775
watanabeyu <fight.ippatu5648@gmail.com>
2018-12-13 16:59:10 +0900

    5.3.2

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,52 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = admin.initializeApp();
+
+const runtimeOpts = {
+  timeoutSeconds: 300,
+  memory: '128MB'
+}
+
+exports.pushSendByLike2 = functions.runWith(runtimeOpts)
+  .firestore
+  .document('user/{fromUID}/liked/{likedPost}')
+  .onCreate(async (snap, context) => {
+    const likedPost = context.params.likedPost;
+
+    const data = snap.data();
+
+    const post = await app.firestore().collection("post").doc(likedPost).get().then(doc => doc.data());
+    const user = await post.user.get().then(doc => doc.data())
+
+    const { deviceToken = null, locale = "ja_jp" } = user
+    if (!deviceToken) {
+      console.log("user don't have deviceToken");
+      return true;
+    }
+
+    const response = fetch('https://exp.host/--/api/v2/push/send', {
+      method: "POST",
+      body: JSON.stringify([{
+        to: deviceToken,
+        badge: 1, // この数字が待受画面でアプリアイコンの右上に赤く出てくる
+        title: (locale === "ja_jp") ? "新しい通知" : "New notification",
+        body: (locale === "ja_jp") ? "いいねされました" : "Your post liked",
+        data: {
+          screen: "NotificationTab"
+        },
+        channelId: "Default"
+      }]),
+      headers: {
+        'content-type': 'application/json',
+      }
+    }).then(res => res.json())
+
+    if (response.errors) {
+      console.error("push failed")
+    } else {
+      console.log("push success")
+    }
+  })
