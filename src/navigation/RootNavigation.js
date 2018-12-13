42f63d3fcd11e7b2f72eb044ab782a11c9a6340e
watanabeyu <fight.ippatu5648@gmail.com>
2018-12-13 16:30:56 +0900

    5.2.1

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { BackHandler } from 'react-native';
+import { Permissions, Notifications } from 'expo';
 import { connect } from 'react-redux';
 import { NavigationActions } from 'react-navigation';
 import { reduxifyNavigator } from 'react-navigation-redux-helpers';
@@ -31,10 +32,32 @@ export default class AppWithNavigationState extends React.Component {
     const { dispatch } = this.props;
     dispatch({ type: 'ME_SET', payload: me });
     this.setState({ loading: false });
+
+    await this.getDeviceToken();
   }
 
   componentWillUnmount() {
     BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  getDeviceToken = async () => {
+    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+      // パーミッションダイアログを表示します
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+      return;
+    }
+
+    const deviceToken = await Notifications.getExpoPushTokenAsync();
+    if (deviceToken) {
+      firebase.updateUserToken(deviceToken);
+    }
   }
 
   onBackPress = () => {
